66d6311d86d4445926f70e6af9f2e995dadbf777
isaiasNsantos <149079069+isaiasNsantos@users.noreply.github.com>
2024-01-23 10:21:40 -0300

    Update tabuada_2.0.py

--- tabuada_2.0.py
+++ tabuada_2.0.py
@@ -1,3 +1,3 @@
-num =  int(input('Digite um número para ver sua tabuada: '))
-for n in range(1, 11, ):
+num = int(input('Digite um número para ver sua tabuada: '))
+for n in range(1, 11):
     print(f'{num} x {n:2} = {num*n}')
